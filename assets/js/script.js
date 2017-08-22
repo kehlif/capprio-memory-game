58e9179d9cb7142dad3df3f49bdf8ad77c334d6b
fatima kehli <fatima.kehili@hotmail.fr>
2017-08-22 11:52:06 +0200

    debut de memory game avec les images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,24 @@
 var Images = [
-  "assets/images/1.jpg", 
-  "assets/images/2.jpg", 
-  "assets/images/3.jpg", 
-  "assets/images/4.jpg", 
-  "assets/images/5.jpg", 
-  "assets/images/6.jpg", 
-  "assets/images/7.jpg", 
+  "assets/images/1.jpg",
+  "assets/images/2.jpg",
+  "assets/images/3.jpg",
+  "assets/images/4.jpg",
+  "assets/images/5.jpg",
+  "assets/images/6.jpg",
+  "assets/images/7.jpg",
   "assets/images/8.jpg",
-  "assets/images/9.jpg", 
+  "assets/images/9.jpg",
   "assets/images/10.jpg",
   "assets/images/guts.png",
   "assets/images/panda.jpg",
 ];
+
+console.log(Images);
+for( i=Images.length -1, i>=1; i--){
+  var a = Math.floor(Math.random()*(i+1));
+
+  var b = Images[i];
+  Images[i] = Images[a];
+  Images[a] = b;
+}
+console.log(Images);
